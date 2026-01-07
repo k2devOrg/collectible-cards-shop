9deb4dd8e9bd454ae6b0c654bb723ba507ca4b70
Konrad <konradk95@gmail.com>
2026-01-07 15:02:57 +0100

    feat: setup app shell, header MVP, marketing routing and shop placeholder

--- frontend/web/src/app/app.routes.ts
+++ frontend/web/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/marketing/marketing.routes')
+        .then(m => m.marketingRoutes)
+  },
+  {
+    path: 'shop',
+    loadComponent: () =>
+      import('./features/shop/shop-page/shop-page')
+        .then(m => m.ShopPageComponent),
+  },
+];
---
+++ frontend/web/src/app/core/layout/app-shell/app-shell/app-shell.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from '../../header/header/header';
+
+@Component({
+  selector: 'app-app-shell',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app-shell.html',
+  styleUrl: './app-shell.css',
+})
+export class AppShellComponent {
+
+}
